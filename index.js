f19c44d04ffdfecdc75c4d30247a492f23bfce84
Divine <ujenezadivine@gmail.com>
2022-03-17 13:55:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+module.exports.add= function(a,b){
+    return a+b 
+}
+module.exports.substract = function(a,b){
+    return a-b
+}
+module.exports.mutiple = function(a,b){
+    return a*b
+}
+module.exports.divide = function(a,b){
+    return a/b
+}
